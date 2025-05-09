0ef6c87035e3ad8643613cd77f4eb7c966fefd77
darooon <jaffdaroon123@gmail.com>
2025-05-09 15:09:13 +1000

    fixed broken imports

--- securekevin/src/App.js
+++ securekevin/src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import FamilyMember from './pages/FamilyMember';
@@ -14,10 +13,9 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
-import Nurse from "./pages/Nurse";
 import NotFound from "./pages/NotFound";
+
 
 // ... keep existing code
 
