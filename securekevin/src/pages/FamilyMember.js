c61960f1677d8fd28ff8fef2de4ba72ec31394b0
darooon <jaffdaroon123@gmail.com>
2025-05-16 14:58:57 +1000

    Family member implementation

--- securekevin/src/pages/FamilyMember.js
+++ securekevin/src/pages/FamilyMember.js
@@ -1,4 +1,3 @@
-/*
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { 
@@ -583,4 +582,3 @@ const FamilyMember = () => {
 };
 
 export default FamilyMember;
-*/
